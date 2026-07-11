2fa8c022fa97ec5cc0266c3fb39eb419a2b9a642
stefanliciu <licius9090@gmail.com>
2026-07-11 20:47:33 +0300

    Add template for the api entrypoint and contracts module

---
+++ contracts/src/main/java/disertatie/contracts/model/Queues.java
@@ -0,0 +1,11 @@
+package disertatie.contracts.model;
+
+public final class Queues {
+
+    private Queues() {}
+
+    public static final String JOBS = "q.analysis.jobs";
+    public static final String STAGE_INGESTION = "q.stage.ingestion";
+    public static final String STAGE_REACHABILITY = "q.stage.reachability";
+    public static final String STAGE_RESULTS = "q.stage.results";
+}
